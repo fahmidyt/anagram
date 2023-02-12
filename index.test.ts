67a9ca12a07037644fd8bd9023c9312a01509005
fahmidyt <fmidyt@gmail.com>
2023-02-12 13:59:47 +0700

    feat: anagram unit test

---
+++ index.test.ts
@@ -0,0 +1,11 @@
+import anagram from "./index";
+
+describe("Testing Anagram", () => {
+  it("Should throw error when prop anagrams is not an array", () => {});
+
+  it("Should return empty array when anagrams list is empty", () => {});
+
+  it("Should return array 2d when anagram is correct", () => {})
+
+  it("Should return correct values when it has anagram", () => {})
+});
