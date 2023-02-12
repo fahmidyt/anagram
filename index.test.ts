4fc39ae72912f5a15081086d78dfe6136b5d76d8
fahmidyt <fmidyt@gmail.com>
2023-02-13 03:51:57 +0700

    refactor: add whitespace test case

--- index.test.ts
+++ index.test.ts
@@ -2,46 +2,68 @@ import anagram from "./anagrams";
 
 describe("Testing Anagram", () => {
   it("Should return empty array when anagrams list is empty", () => {
-    const result = anagram([])
+    const result = anagram([]);
 
-    expect(Array.isArray(result)).toBe(true)
-    expect(result.length).toEqual(0)
-    expect(result).toStrictEqual([])
+    expect(Array.isArray(result)).toBe(true);
+    expect(result.length).toEqual(0);
+    expect(result).toStrictEqual([]);
   });
 
   it("Should return array 2d when anagram is correct", () => {
-    const result = anagram(['aku', 'kua'])
+    const result = anagram(["aku", "kua"]);
 
-    expect(result.length).toEqual(1)
-    expect(Array.isArray(result[0])).toEqual(true)
-  })
+    expect(result.length).toEqual(1);
+    expect(Array.isArray(result[0])).toEqual(true);
+  });
 
   it("Should Ignore case of the letters and should doesn't affect the comparison.", () => {
-    const anagrams = ['aKu', 'Kua', 'kUa']
+    const anagrams = ["aKu", "Kua", "kUa"];
 
-    const result = anagram(anagrams)
+    const result = anagram(anagrams);
 
-    expect(result.length).toEqual(1)
-    expect(Array.isArray(result[0])).toEqual(true)
-    expect(result[0].length).toEqual(3)
+    expect(result.length).toEqual(1);
+    expect(Array.isArray(result[0])).toEqual(true);
+    expect(result[0].length).toEqual(3);
 
-    expect((result[0] as string[]).sort()).toEqual(anagrams.sort())
-  })
+    expect((result[0] as string[]).sort()).toEqual(anagrams.sort());
+  });
 
   it("Should return correct values when it has anagram", () => {
-    const result = anagram(['aku', 'kua', 'kamu', 'muka'])
+    const result = anagram(["aku", "kua", "kamu", "muka"]);
 
     // check parent array
-    expect(result.length).toStrictEqual(2)
+    expect(result.length).toStrictEqual(2);
 
     // check first anagram value
-    expect(Array.isArray(result[0])).toEqual(true)
-    expect(result[0].length).toStrictEqual(2)
-    ;(result[0] as string[]).map(v => ['aku', 'kua'].includes(v))
+    expect(Array.isArray(result[0])).toEqual(true);
+    expect(result[0].length).toStrictEqual(2);
+    (result[0] as string[]).map((v) => ["aku", "kua"].includes(v));
 
     // check second anagram value
-    expect(Array.isArray(result[1])).toEqual(true)
-    expect(result[1].length).toStrictEqual(2)
-    ;(result[1] as string[]).map(v => ['kamu', 'muka'].includes(v))
-  })
+    expect(Array.isArray(result[1])).toEqual(true);
+    expect(result[1].length).toStrictEqual(2);
+    (result[1] as string[]).map((v) => ["kamu", "muka"].includes(v));
+  });
+
+  it("Should ignore white spaces", () => {
+    const result = anagram([
+      "gold and silver",
+      "grand old evils",
+      "Statue of Liberty",
+      "built to stay free",
+    ]);
+
+    // check parent array
+    expect(result.length).toStrictEqual(2);
+
+    // check first anagram value
+    expect(Array.isArray(result[0])).toEqual(true);
+    expect(result[0].length).toStrictEqual(2);
+    (result[0] as string[]).map((v) => ["aku", "kua"].includes(v));
+
+    // check second anagram value
+    expect(Array.isArray(result[1])).toEqual(true);
+    expect(result[1].length).toStrictEqual(2);
+    (result[1] as string[]).map((v) => ["kamu", "muka"].includes(v));
+  });
 });
