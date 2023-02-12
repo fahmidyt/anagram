fbfe54b4e555a101048d9a19f7de6ff998cc5c47
fahmidyt <fmidyt@gmail.com>
2023-02-12 14:23:41 +0700

    feat: add unit test anagram logic

--- index.test.ts
+++ index.test.ts
@@ -1,10 +1,35 @@
 import anagram from "./index";
 
 describe("Testing Anagram", () => {
+  it("Should return empty array when anagrams list is empty", () => {
+    const result = anagram([])
 
-  it("Should return empty array when anagrams list is empty", () => {});
+    expect(Array.isArray(result)).toBe(true)
+    expect(result.length).toEqual(0)
+    expect(result).toStrictEqual([])
+  });
 
-  it("Should return array 2d when anagram is correct", () => {})
+  it("Should return array 2d when anagram is correct", () => {
+    const result = anagram(['aku', 'kua'])
 
-  it("Should return correct values when it has anagram", () => {})
+    expect(result.length).toEqual(1)
+    expect(Array.isArray(result[0])).toEqual(true)
+  })
+
+  it("Should return correct values when it has anagram", () => {
+    const result = anagram(['aku', 'kua', 'kamu', 'muka'])
+
+    // check parent array
+    expect(result.length).toStrictEqual(2)
+
+    // check first anagram value
+    expect(Array.isArray(result[0])).toEqual(true)
+    expect(result[0].length).toStrictEqual(2)
+    ;(result[0] as string[]).map(v => ['aku', 'kua'].includes(v))
+
+    // check second anagram value
+    expect(Array.isArray(result[1])).toEqual(true)
+    expect(result[1].length).toStrictEqual(2)
+    ;(result[1] as string[]).map(v => ['kamu', 'muka'].includes(v))
+  })
 });
